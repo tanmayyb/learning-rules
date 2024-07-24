8916aa0a80cf73b274246003eb297412315e49f9
Joshua Tindall <114206223+joshuatindall@users.noreply.github.com>
2024-07-24 15:15:37 -0400

    Create BasicOptim.py
    Added basic optimizer class

---
+++ rules/classes/BasicOptim.py
@@ -0,0 +1,43 @@
+class BasicOptimizer(torch.optim.Optimizer):
+  """
+  Simple optimizer class based on the SGD optimizer.
+  """
+  def __init__(self, params, lr=0.01, weight_decay=0):
+    """
+    Initializes a basic optimizer object.
+
+    Arguments:
+    - params (generator): Generator for torch model parameters.
+    - lr (float, optional): Learning rate.
+    - weight_decay (float, optional): Weight decay.
+    """
+
+    if lr < 0.0:
+        raise ValueError(f"Invalid learning rate: {lr}")
+    if weight_decay < 0.0:
+        raise ValueError(f"Invalid weight_decay value: {weight_decay}")
+
+    defaults = dict(
+        lr=lr,
+        weight_decay=weight_decay,
+        )
+
+    super().__init__(params, defaults)
+
+  def step(self):
+      """
+      Performs a single optimization step.
+      """
+
+      for group in self.param_groups:
+        for p in group["params"]:
+
+          # only update parameters with gradients
+          if p.grad is not None:
+
+            # apply weight decay to gradient, if applicable
+            if group["weight_decay"] != 0:
+              p.grad = p.grad.add(p, alpha=group["weight_decay"])
+
+            # apply gradient-based update
+            p.data.add_(p.grad, alpha=-group["lr"])

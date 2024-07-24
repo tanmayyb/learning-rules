b2abe0a635ca4376a60f527bc63cdbfa6b1b3ea4
Tanmay Bishnoi <tbishnoi@torontomu.ca>
2024-07-24 15:57:47 -0400

    trainer.py like the training notebook

--- trainer.py
+++ trainer.py
@@ -2,160 +2,162 @@ import torch
 from torch.utils.data import DataLoader
 from torchvision import datasets, transforms
 from torch.autograd import Variable
-
 from tqdm import tqdm
 from datetime import datetime, timezone
-
-# Get the current date and time
-
+import os
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
-batch_size = 32
-epochs = 2
-rule_select = 'hebb'
-num_inputs = 784
-num_hidden = 100
-num_outputs = 10
-activation_type = 'relu'
-bias = False
-lr=1e-4
-momentum=0.9
-weight_decay=0.001
-nesterov=True
 
+# !pip install wandb --quiet
 import wandb
-from wandb_config import USERNAME
+wandb.login()
+
+
+
+
+entity="adorable-lantanas"
+project="learning-rules"
 
 configs = dict(
-    rule_select = rule_select,
-    num_inputs = num_inputs,
-    num_hidden = num_hidden,
-    num_outputs = num_outputs,
-    activation_type = activation_type,
-    bias = bias,
-    lr = lr,
-    momentum = momentum,
-    weight_decay = weight_decay,
-    nesterov = nesterov,
+  rule_select = 'hebb',
+  epochs = 2,
+  batch_size = 32,
+  num_inputs = 784,
+  num_hidden = 100,
+  num_outputs = 10,
+  activation_type = 'relu',
+  bias = False,
+  lr=1e-4,
+  momentum=0.9,
+  weight_decay=0.001,
+  nesterov=True,
+  
+  ############
+  # specific #
+  ############
+
+  # hebbian
+  clamp_output = False,
 )
 
-experiment_name = "hebb-test"
 
-now = datetime.now(timezone.utc)
-formatted_datetime = now.strftime("%y%m%d-%H%M")
-run = wandb.init(
-    entity=USERNAME,
-    project="learning-rules",
-    name=experiment_name+"-"+formatted_datetime,
+train_loader = DataLoader(
+  datasets.MNIST(
+    './data', train=True, download=True,
+    transform = transforms.Compose([
+      transforms.ToTensor(),
+      transforms.Normalize(
+          (0.1307,),(0.3081,)
+      )
+    ])
+  ),
+  batch_size=configs['batch_size'],
+  shuffle=True,    
+)
+
+test_loader = DataLoader(
+  datasets.MNIST(
+    './data', train=False, download=True,
+    transform=transforms.Compose([
+      transforms.ToTensor(),
+        transforms.Normalize(
+          (0.1307,),(0.3081,)
+          )        
+      ]),
+  ),
+  batch_size=configs['batch_size'],
+  shuffle=True,
+)
+
+
+def generate_experiment_name( # write this function to generate your custom name
+    name,
+    # ... any args you want
+):
+  # ... your experiment name generator
+  return name
+
+experiment_name = generate_experiment_name(
+    "<dev>-<experiment>-<counter>",
+    # "tbishnoi-colabtraintest-0",
+    # any other args you want
+)
+
+
+
+def select_model(configs)-> torch.nn.Module:
+  if configs['rule_select'] == 'hebb':
+    from rules.Hebbian import HebbianNetwork
+    model = HebbianNetwork(
+      num_inputs=configs['num_inputs'],
+      num_hidden=configs['num_hidden'],
+      num_outputs=configs['num_outputs'],
+      clamp_output=configs['clamp_output'],
+      bias=configs['bias'],
+    ).to(device)
+
+  elif configs['rule_select'] == 'fa':
+    from rules.FA import FeedbackAlignmentPerceptron
+    model = FeedbackAlignmentPerceptron(
+      num_inputs=configs['num_inputs'],
+      num_hidden=configs['num_hidden'],
+      num_outputs=configs['num_outputs'],
+      bias=configs['bias'],
+      activation_type=configs['activation_type'],
+    ).to(device)
+
+  else:
+      raise NotImplementedError("Selected Rule does not exist!")
+  
+  return model
+
+model = select_model(configs)
+
+
+def trainer(experiment_name, configs):
+  # initialize run
+  run = wandb.init(
+    entity=entity,
+    project=project,
+    name=experiment_name,
     config=configs,
     resume="never",
-)
+  )
 
-
-
-
-# download/load train dataset
-train_loader = DataLoader(
-    datasets.MNIST(
-        './data', train=True, download=True,
-        transform = transforms.Compose([
-            transforms.ToTensor(),
-            transforms.Normalize(
-                (0.1307,),(0.3081,)
-            )
-        ])
-    ),
-    batch_size=BATCH_SIZE,
-    shuffle=True,    
-)
-
-# download/load train dataset
-test_loader = DataLoader(
-    datasets.MNIST(
-        './data', train=False, download=True,
-        transform=transforms.Compose([
-            transforms.ToTensor(),
-            transforms.Normalize(
-                (0.1307,),(0.3081,)
-            )        
-        ]),
-    ),
-    batch_size=BATCH_SIZE,
-    shuffle=True,
-)
-
-
-
-if rule_select == 'hebb':
-    from rules.Hebbian import HebbianNetwork
-
-    model = HebbianNetwork(
-        num_inputs=num_inputs,
-        num_hidden=num_hidden,
-        num_outputs=num_outputs,
-        clamp_output=False,
-        bias=bias,
-    ).to(device)
-
-elif rule_select == 'fa':
-    from rules.FA import FeedbackAlignmentPerceptron
-
-    model = FeedbackAlignmentPerceptron(
-        num_inputs=num_inputs,
-        num_hidden=num_hidden,
-        num_outputs=num_outputs,
-        bias=bias,
-        activation_type=activation_type,
-    ).to(device)
-
-# elif rule_select == ''
-
-else:
-    raise NotImplementedError("Selected Rule does not exist!")
-
-
-
-
-# def train_model(model, train_loader.)
-
-
-optimizer = torch.optim.SGD(
+  optimizer = torch.optim.SGD(
     model.parameters(),
-    lr=lr, momentum=momentum, 
-    weight_decay=weight_decay, 
-    nesterov=nesterov,
-)
-loss_crossentropy = torch.nn.CrossEntropyLoss()
+    lr=configs['lr'], 
+    momentum=configs['momentum'], 
+    weight_decay=configs['weight_decay'], 
+    nesterov=configs['nesterov'],
+  )
+  loss_crossentropy = torch.nn.CrossEntropyLoss()
 
-
-
-for epoch in tqdm(range(epochs)):
+  # training loop
+  for epoch in tqdm(range(configs['epochs'])):
     for idx_batch, (inputs, targets) in enumerate(train_loader):
-        inputs = inputs.view(BATCH_SIZE, -1)
+      inputs = inputs.view(configs['batch_size'], -1)
 
-        # autograd vars
-        inputs, targets = Variable(inputs), Variable(targets)
+      inputs, targets = Variable(inputs), Variable(targets)
+      outputs = model(inputs.to(device))
+      loss = loss_crossentropy(outputs, targets.to(device))
 
-        outputs = model(inputs.to(device))
+      model.zero_grad()
+      loss.backward()
+      optimizer.step()
 
-        loss = loss_crossentropy(outputs, targets.to(device))
-
-        model.zero_grad()
-        loss.backward()
-        optimizer.step()
-
-        run.log({"loss": loss.item(), "epoch": epoch})
+      # training history
+      run.log({"loss": loss.item(), "epoch": epoch})
 
 
-# Save the model
-torch.save(model.state_dict(), "models/model.pth")
-wandb.save("models/model.pth")
+  # save model info
+  os.makedirs("models", exist_ok=True)
+  torch.save(model.state_dict(), "models/model.pth")
+  wandb.save("models/model.pth")
+  
+  # important: finish experiment
+  run.finish()
 
-# run.log_artifact("models/model.pth", name = "state_dict", type = "dict" )
-
-run.finish()
-
-
-
+trainer(experiment_name, configs)
 

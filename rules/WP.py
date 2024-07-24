cff5b937e060cc9092c4462cdc16f63a90bc800c
Joshua Tindall <114206223+joshuatindall@users.noreply.github.com>
2024-07-24 14:08:35 -0400

    Update WP.py
    Fixed wrong learning rate

--- rules/WP.py
+++ rules/WP.py
@@ -8,7 +8,7 @@ class WeightPerturbMLP(MultiLayerPerceptron):
 
   def __init__(self, sigma=1e-4, **kwargs):
     """
-    NOTE: working hyperparameters seem to be lr=1e-3, sigma=1e-4, activation='relu'
+    NOTE: working hyperparameters seem to be lr=1e-4, sigma=1e-4, activation='relu'
     """
 
     self.sigma = sigma

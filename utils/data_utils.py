7922ce0e93b04d1a7d4c542e6afca1150abe2a92
Joshua Tindall <114206223+joshuatindall@users.noreply.github.com>
2024-07-24 14:54:30 -0400

    Create data_utils.py
    Added utils for loading and manipulating mnist data

---
+++ utils/data_utils.py
@@ -0,0 +1,68 @@
+def download_mnist(train_prop=0.8, keep_prop=0.5):
+
+  valid_prop = 1 - train_prop
+
+  discard_prop = 1 - keep_prop
+
+  transform = torchvision.transforms.Compose(
+      [torchvision.transforms.ToTensor(),
+      torchvision.transforms.Normalize((0.1307,), (0.3081,))]
+      )
+
+
+  with contextlib.redirect_stdout(io.StringIO()): #to suppress output
+
+      full_train_set = torchvision.datasets.MNIST(
+          root="./data/", train=True, download=True, transform=transform
+          )
+      full_test_set = torchvision.datasets.MNIST(
+          root="./data/", train=False, download=True, transform=transform
+          )
+
+  train_set, valid_set, _ = torch.utils.data.random_split(
+      full_train_set,
+      [train_prop * keep_prop, valid_prop * keep_prop, discard_prop]
+      )
+  test_set, _ = torch.utils.data.random_split(
+      full_test_set,
+      [keep_prop, discard_prop]
+      )
+
+  print("Number of examples retained:")
+  print(f"  {len(train_set)} (training)")
+  print(f"  {len(valid_set)} (validation)")
+  print(f"  {len(test_set)} (test)")
+
+  return train_set, valid_set, test_set
+
+def restrict_classes(dataset, classes=[6], keep=True):
+  """
+  Removes or keeps specified classes in a dataset.
+
+  Arguments:
+  - dataset (torch dataset or subset): Dataset with class targets.
+  - classes (list): List of classes to keep or remove.
+  - keep (bool): If True, the classes specified are kept. If False, they are
+  removed.
+
+  Returns:
+  - new_dataset (torch dataset or subset): Datset restricted as specified.
+  """
+
+  if hasattr(dataset, "dataset"):
+    indices = np.asarray(dataset.indices)
+    targets = dataset.dataset.targets[indices]
+    dataset = dataset.dataset
+  else:
+    indices = np.arange(len(dataset))
+    targets = dataset.targets
+
+  specified_idxs = np.isin(targets, np.asarray(classes))
+  if keep:
+    retain_indices = indices[specified_idxs]
+  else:
+    retain_indices = indices[~specified_idxs]
+
+  new_dataset = torch.utils.data.Subset(dataset, retain_indices)
+
+  return new_dataset
